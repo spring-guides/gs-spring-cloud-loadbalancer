7833304edfa20e2b2a0cb4039cf39ed2f7697a80
Stéphane Nicoll <stephane.nicoll@broadcom.com>
2026-02-06 19:52:10 +0100

    Add empty applications in initial/
    Closes gh-16

---
+++ initial/say-hello/src/main/java/hello/SayHelloApplication.java
@@ -0,0 +1,13 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SayHelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SayHelloApplication.class, args);
+	}
+
+}
